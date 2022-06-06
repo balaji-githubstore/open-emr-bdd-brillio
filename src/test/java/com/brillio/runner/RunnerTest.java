bff5ad9d9675d12bee03e1156d55b142f954c020
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-06-06 09:28:57 +0530

    step defn created

--- src/test/java/com/brillio/runner/RunnerTest.java
+++ src/test/java/com/brillio/runner/RunnerTest.java
@@ -5,7 +5,10 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-@CucumberOptions(features = {"src/test/resources/feature/Login.feature"})
+@CucumberOptions(
+		features = {"src/test/resources/feature/Login.feature"}
+		,glue = {"com.brillio.stepdefinitions"}
+		)
 
 @RunWith(Cucumber.class)
 public class RunnerTest {
---
+++ src/test/java/com/brillio/stepdefinitions/LoginSteps.java
@@ -0,0 +1,62 @@
+package com.brillio.stepdefinitions;
+
+import java.time.Duration;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginSteps {
+	WebDriver driver;
+
+	@Given("I have browser with openemr application")
+	public void i_have_browser_with_openemr_application() {
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://demo.openemr.io/b/openemr");
+	}
+
+	@When("I enter username as {string}")
+	public void i_enter_username_as(String username) {
+		driver.findElement(By.id("authUser")).sendKeys(username);
+	}
+
+	@When("I enter password as {string}")
+	public void i_enter_password_as(String password) {
+		driver.findElement(By.id("clearPass")).sendKeys(password);
+	}
+
+	@When("I select the language as {string}")
+	public void i_select_the_language_as(String language) {
+		Select selectLan=new Select(driver.findElement(By.xpath("//select[@name='languageChoice']")));
+		selectLan.selectByVisibleText(language);
+	}
+
+	@When("I click on login")
+	public void i_click_on_login() {
+		driver.findElement(By.cssSelector("#login-button")).click();
+	}
+
+	@Then("I should get access to the portal with title as {string}")
+	public void i_should_get_access_to_the_portal_with_title_as(String expectedTitle) {
+		
+		WebDriverWait wait =new WebDriverWait(driver, Duration.ofSeconds(50));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[text()='Patient']")));
+		
+		Assert.assertEquals(expectedTitle,driver.getTitle());
+		
+	}
+
+}
